11f8954a8333dc87ba38c51f76f8c4effd658244
dinb1242 <39480072+dinb1242@users.noreply.github.com>
2021-09-13 17:38:27 +0900

    0913_버그 수정 - 라이브러리 추가 모달 미동작 수정
    ★이게 왜 되지?

--- public/js/cdn_script.js
+++ public/js/cdn_script.js
@@ -7,6 +7,10 @@ document.write('<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></s
 // document.write("<script src='https://cdn.jsdelivr.net/pyodide/v0.18.0/full/pyodide.js'></script>")
 document.write("<script src='https://cdn.jsdelivr.net/pyodide/v0.18.0/full/pyodide.js'></script>")
 
+// Semantic UI
+document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js' integrity='sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw==' crossorigin='anonymous'></script>");
+document.write("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css' integrity='sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==' crossorigin='anonymous' />");
+
 // Bootstrap
 document.write("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css'>");
 document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js'></script>");
@@ -15,11 +19,6 @@ document.write("<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/
 
 // Blockly?
 document.write("<script src='https://unpkg.com/prop-types@15.6/prop-types.js'></script>");
-
-// Semantic UI
-document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js' integrity='sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw==' crossorigin='anonymous'></script>");
-document.write("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css' integrity='sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==' crossorigin='anonymous' />");
-
 
 {/* <!-- plotly --> */ }
 document.write("<script src='https://cdn.plot.ly/plotly-latest.min.js' charset='utf-8'></script>")
