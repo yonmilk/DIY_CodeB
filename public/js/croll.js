3431c734ea8d7791bc4f63fcb2eeb2d9f46ca953
UniM0cha <solst_ice@naver.com>
2021-09-14 15:43:05 +0900

    0914_크롤링 버그 패치 진행 - 크롬에서 가능하게 구현
    *추가내용
    .DS_Store gitignore 추가
    ...맥송합니다..

--- public/js/croll.js
+++ public/js/croll.js
@@ -13,24 +13,18 @@ function _requestsURL(_url) {
       
       // 크롬에서는 버퍼 크기 문제로 큰 데이터를 옮기지 못함
       // 1000개씩 잘라서 차례차례 보낸다
-      let pythonCode = `subHtmlData = ""`
+      let pythonCode = `
+      subHtmlData = ""
+      req = ""`
       pyodide.runPython(pythonCode);
 
       for (let i = 0; i < htmlData.length; i += 1000) {
         let subHtmlData = htmlData.substr(i, 1000);
-        // console.log(subHtmlData);     // 1000개씩 잘린 코드를 출력해본다.
+        // console.log(subHtmlData);     // 1000개씩 잘린 코드를 출력해본다. -> 너무 많네ㅎ
         pyodide.globals.set('subHtmlData', subHtmlData);
         pythonCode = `req += subHtmlData`;
         pyodide.runPython(pythonCode);
       }
-      
-      /*
-      해결해야 할 문제
-      req에 데이터가 누적된다.
-      실행버튼을 두번 눌러야 실행된다.
-      */
-
-      // pyodide.globals.set('req', data.data); // node에서 받아온 데이터를 htmldata 전역변수에 넣어준다. (차후 크롤링블록에서 htmldata변수가 사용됨)
     },
   });
 }
