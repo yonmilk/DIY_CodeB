67c39d9814c31928c35cafd3d6c4115b13f58196
UniM0cha <solst_ice@naver.com>
2021-09-28 00:05:16 +0900

    0927_탭 리팩토링+전체크기 조절 - Description 참고
    1. 전체화면일 때 스크롤 안해도 되는 크기로 조절 (가로 길이룰 줄이면 상단이 넓어지면서 내려오는 것은 나중에 수정해야함)
    2. Workspace1,2,3 으로 나누어져 있던것 workspace로 통합하여 리팩토링
    3. 그와 동시에 센터,+,- 버튼 버그 수정

--- public/js/utils/codeMirror.js
+++ public/js/utils/codeMirror.js
@@ -43,25 +43,8 @@ let asyncEditor = CodeMirror(document.getElementById("asyncCodeEditor"), {
 //---------------------------------------예원 수정-----------------------------------------
 function codeUpdate(event) {
 	async function coderun() {
-        //예원
-        let tab = document.getElementsByClassName('tab-link current'); 
-	    let tab_id = tab[0].firstElementChild.id;
-
 	try {
-        //예원
-        var pyCode;
-        if(tab_id == 'tab_1') {
-            pyCode = Blockly.Python.workspaceToCode(Workspace1);
-        
-        } else if(tab_id == 'tab_2') {
-            pyCode = Blockly.Python.workspaceToCode(Workspace2);
-        
-        } else if(tab_id == 'tab_3') {
-            pyCode = Blockly.Python.workspaceToCode(Workspace3);
-        
-        }
-
-		// var pyCode = Blockly.Python.workspaceToCode(demoWorkspace); // 워크스페이스를 코드로 변환 후 값 저장
+		var pyCode = Blockly.Python.workspaceToCode(workspace); // 워크스페이스를 코드로 변환 후 값 저장
 		// console.log("pycode : " + escape(pyCode));
 		pyEditor.setValue(pyCode); // 그 값을 파이썬코드미러에 값 저장
 	} catch (e) {
@@ -71,20 +54,7 @@ function codeUpdate(event) {
 	// coderun이 끝나고나서 작동됨.
 	coderun().then(() => {
 	try {
-        //예원
-        var jsCode;
-        if(tab_id == 'tab_1') {
-            jsCode = Blockly.JavaScript.workspaceToCode(Workspace1);
-        
-        } else if(tab_id == 'tab_2') {
-            jsCode = Blockly.JavaScript.workspaceToCode(Workspace2);
-        
-        } else if(tab_id == 'tab_3') {
-            jsCode = Blockly.JavaScript.workspaceToCode(Workspace3);
-        
-        }
-
-		// var jsCode = Blockly.JavaScript.workspaceToCode(demoWorkspace); // 워크스페이스를 js코드로 변환 후 값 저장
+		var jsCode = Blockly.JavaScript.workspaceToCode(workspace); // 워크스페이스를 js코드로 변환 후 값 저장
 		jsEditor.setValue(jsCode); // 그 값을 js코드미러에 값 저장
 	} catch (e) {
 		console.log('java script 코드가 매핑되어 있지 않습니다. Error : ' + e);
