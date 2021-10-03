bb7b1f8bb9ceffdafa60326aa783c21caf72fe8d
kyh174 <kyh174@naver.com>
2021-10-04 00:01:43 +0900

    1004_연산블록 수정 - 설명참조
    숫자(Num)&문자(Str)
    - 0 + 0 모양 연산 블록 괄호 문제 수정
    - 기존 코드 이해 불가로 새로 블록을 만들었음

--- public/js/blockly/javascript_compressed.js
+++ public/js/blockly/javascript_compressed.js
@@ -475,22 +475,34 @@ Blockly.JavaScript.math_number = function (a) {
     a = Number(a.getFieldValue("NUM"));
     return [a, 0 <= a ? Blockly.JavaScript.ORDER_ATOMIC : Blockly.JavaScript.ORDER_UNARY_NEGATION]
 };
-Blockly.JavaScript.math_arithmetic = function (a) {
-    var b = {
-            ADD: [" + ", Blockly.JavaScript.ORDER_ADDITION],
-            MINUS: [" - ", Blockly.JavaScript.ORDER_SUBTRACTION],
-            MULTIPLY: [" * ", Blockly.JavaScript.ORDER_MULTIPLICATION],
-            DIVIDE: [" / ", Blockly.JavaScript.ORDER_DIVISION],
-            REMAINDER: [" % ", Blockly.JavaScript.ORDER_MULTIPLICATIVE],
-            POWER: [null, Blockly.JavaScript.ORDER_COMMA],
-            QUOTIENT: [null, Blockly.JavaScript.ORDER_COMMA]
-        } [a.getFieldValue("OP")],
-        c = b[0];
-    b = b[1];
-    var d = Blockly.JavaScript.valueToCode(a, "A", b) || "0";
-    a = Blockly.JavaScript.valueToCode(a, "B", b) || "0";
-    return c ? [d + c + a, b] : ["Math.pow(" + d + ", " + a + ")", Blockly.JavaScript.ORDER_FUNCTION_CALL]
-};
+// 연산 블록 코드 이해 불가로 새로 작성
+// Blockly.JavaScript.math_arithmetic = function (a) {
+//     var b = {
+//             ADD: [" + ", Blockly.JavaScript.ORDER_ADDITION],
+//             MINUS: [" - ", Blockly.JavaScript.ORDER_SUBTRACTION],
+//             MULTIPLY: [" * ", Blockly.JavaScript.ORDER_MULTIPLICATION],
+//             DIVIDE: [" / ", Blockly.JavaScript.ORDER_DIVISION],
+//             REMAINDER: [" % ", Blockly.JavaScript.ORDER_MULTIPLICATIVE],
+//             POWER: [null, Blockly.JavaScript.ORDER_COMMA],
+//             QUOTIENT: [null, Blockly.JavaScript.ORDER_COMMA]
+//         } [a.getFieldValue("OP")],
+//         c = b[0];
+//     b = b[1];
+//     var d = Blockly.JavaScript.valueToCode(a, "A", b) || "0";
+//     a = Blockly.JavaScript.valueToCode(a, "B", b) || "0";
+//     return c ? [d + c + a, b] : ["Math.pow(" + d + ", " + a + ")", Blockly.JavaScript.ORDER_FUNCTION_CALL]
+// };
+
+// 연산 블록
+Blockly.JavaScript['math_arithmetic'] = function(block) {
+    var value_a = Blockly.Python.valueToCode(block, 'A', Blockly.Python.ORDER_ATOMIC);
+    var value_b = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_ATOMIC);
+    var dropdown_op = block.getFieldValue('OP');
+    // TODO: Assemble Python into code variable.
+    var code = `${value_a} ${dropdown_op} ${value_b}`;
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_ATOMIC];
+  };
 
 
 Blockly.JavaScript.math_single = function (a) {
